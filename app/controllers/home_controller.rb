ee1a4019be2be39843b0a3170fa64fc6d901e956
qodirz <aq.abdulqodir@gmail.com>
2020-03-17 13:25:54 +0700

    add survey page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def survey
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get '/survey', to: "home#survey"
   root to: "home#index"
 end
